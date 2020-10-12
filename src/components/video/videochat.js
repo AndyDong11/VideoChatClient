800c075d3a8179ff753342bbfd5141723d0b98f7
AndyDong11 <66645390+AndyDong11@users.noreply.github.com>
2020-10-12 17:26:22 -0400

    fixed signaling

--- src/components/video/videochat.js
+++ src/components/video/videochat.js
@@ -2,10 +2,19 @@ import React, { useEffect, useRef } from 'react'
 import io from 'socket.io-client'
 
 let socket = io('http://localhost:3001')
+const configuration = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] }
+const peerConnection = new RTCPeerConnection(configuration);
+
+const mediaConstraints = {
+    'offerToReceiveAudio': true,
+    'offerToReceiveVideo': true
+};
+
 
 export default function VideoChat(props) {
 
     const videoRef = useRef()
+    const localRef = useRef()
 
     useEffect(() => {
         makeCall()
@@ -13,9 +22,18 @@ export default function VideoChat(props) {
 
     async function makeCall() {
 
-        const configuration = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] }
-        const peerConnection = new RTCPeerConnection(configuration);
         const remoteStream = new MediaStream();
+
+        const localStream = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: { facingMode: "environment" },
+        });
+
+        localRef.current.srcObject = localStream
+
+        localStream.getTracks().forEach(track => {
+            peerConnection.addTrack(track, localStream);
+        });
 
         peerConnection.addEventListener('icecandidate', event => {
             if (event.candidate) {
@@ -26,12 +44,10 @@ export default function VideoChat(props) {
         peerConnection.addEventListener('connectionstatechange', event => {
             if (peerConnection.connectionState === 'connected') {
                 console.log('peers connected')
-                localStream.getTracks().forEach(track => {
-                    peerConnection.addTrack(track, localStream);
-                });
             }
         });
         peerConnection.addEventListener('track', async (event) => {
+            console.log('added track')
             remoteStream.addTrack(event.track, remoteStream);
         });
 
@@ -48,7 +64,7 @@ export default function VideoChat(props) {
             if (message.answer) {
                 console.log('recieved an answer')
                 const remoteDesc = new RTCSessionDescription(message.answer);
-                await peerConnection.setRemoteDescription(remoteDesc);
+                peerConnection.setRemoteDescription(remoteDesc)
             }
             else {
                 console.log('no answer')
@@ -67,24 +83,25 @@ export default function VideoChat(props) {
             }
         });
 
-        const offer = await peerConnection.createOffer();
+        videoRef.current.srcObject = remoteStream;
+    }
+
+    let startCall = async () => {
+        const offer = await peerConnection.createOffer(mediaConstraints);
         await peerConnection.setLocalDescription(offer);
         socket.emit('offer', offer);
-
-        const localStream = await navigator.mediaDevices.getUserMedia({
-            audio: false,
-            video: { facingMode: "environment" },
-        });
-
-        videoRef.current.srcObject = remoteStream;
-
-
     }
 
     return (
         <div>
             <div>
                 VideoChat
+            </div>
+            <div>
+                <button onClick={startCall}>Start</button>
+            </div>
+            <div>
+                <video ref={localRef} autoPlay playsInline controls={false} mute />
             </div>
             <div>
                 <video ref={videoRef} autoPlay playsInline controls={false} mute />
